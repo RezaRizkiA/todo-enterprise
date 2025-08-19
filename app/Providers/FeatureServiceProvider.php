395bea55b95d363f8d76a8a9933c2d3a7a6dceea
Reza Rizki A <rezariski.akbar@gmail.com>
2025-08-19 20:38:33 +0700

    feat: introduce UserRepository (contract + eloquent impl) and bind in provider

--- app/Providers/FeatureServiceProvider.php
+++ app/Providers/FeatureServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\Eloquent\EloquentUserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class FeatureServiceProvider extends ServiceProvider
---
+++ app/Repositories/Eloquent/EloquentUserRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\User;
+use App\Repositories\Contracts\UserRepositoryInterface;
+
+class EloquentUserRepository implements UserRepositoryInterface
+{
+    public function create(array $data): User
+    {
+        return User::create($data);
+    }
+
+    public function findByEmail(string $email): ?User
+    {
+        return User::where('email', $email)->first();
+    }
+
+    public function updatePassword(User $user, string $hashed): void
+    {
+        $user->forceFill(['password' => $hashed])->save();
+    }
+}
